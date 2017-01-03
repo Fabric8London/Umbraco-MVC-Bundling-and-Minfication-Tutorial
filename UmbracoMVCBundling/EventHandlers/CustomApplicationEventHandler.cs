7caedc7a100f72b1be27562ca4c6dd8298809e8c
JimPlimmer <jim.plimmer@fabric8.co.uk>
2017-01-03 15:04:16 +0000

    EventHandler
    Commiting

---
+++ UmbracoMVCBundling/EventHandlers/CustomApplicationEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using Umbraco.Core;
+
+namespace UmbracoMVCBundling.EventHandlers
+{
+    public class CustomApplicationEventHandler : ApplicationEventHandler
+    {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+    
+}
---
+++ UmbracoMVCBundling/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UmbracoMVCBundling.Startup))]
+namespace UmbracoMVCBundling
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
